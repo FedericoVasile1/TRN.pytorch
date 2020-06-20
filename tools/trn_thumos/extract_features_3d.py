cd29a248988ec1d926a292b0cfec129db5670e98
FedericoVasile1 <50639319+FedericoVasile1@users.noreply.github.com>
2020-06-20 15:49:57 +0200

    fix 3d feat extr

--- tools/trn_thumos/extract_features_3d.py
+++ tools/trn_thumos/extract_features_3d.py
@@ -22,20 +22,18 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     model = models.video.r2plus1d_18(pretrained=True)
-    FEAT_VECT_DIM = model.fc.in_features        # == 512
+    FEAT_VECT_DIM = model.fc.in_features
     model.fc = nn.Identity()
 
     model = model.to(device)
     model.train(False)
 
     transform = transforms.Compose([
-        #transforms.Resize((112, 112)),
         transforms.ToTensor(),
         transforms.Normalize([0.43216, 0.394666, 0.37645], [0.22803, 0.22145, 0.216989])
     ])
 
-    CHUNK_SIZE = 6     # generate a feature vector every CHUNK_SIZE frames
-    BATCH_SIZE = 32    # number of chunks that will be fed simultaneously to the model i.e. forward pass
+    SAMPLE_FRAMES = 6     # generate a feature vector every SAMPLE_FRAMES frames
 
     DATA_ROOT = 'data/THUMOS'
     VIDEO_FRAMES = 'video_frames_24fps'   # base folder where the video folders (containing the frames) are
@@ -46,41 +44,21 @@ def main():
         videos_dir = [dir for dir in videos_dir if 'video' in dir]
         for dir in videos_dir:
             num_frames = len(os.listdir(os.path.join(DATA_ROOT, VIDEO_FRAMES, dir)))
-            num_frames = num_frames - (num_frames % CHUNK_SIZE)
+            num_frames = num_frames - (num_frames % SAMPLE_FRAMES)
 
-            feat_vects_video = torch.zeros(num_frames//CHUNK_SIZE, FEAT_VECT_DIM, dtype=torch.float32)
-            batch = torch.zeros(BATCH_SIZE, CHUNK_SIZE, 3, 180, 320, dtype=torch.float32)
-            sample = torch.zeros(CHUNK_SIZE, 3, 112, 112, dtype=torch.float32)
-            count_sample = 0
+            feat_vects_video = torch.zeros(num_frames//SAMPLE_FRAMES, FEAT_VECT_DIM, dtype=torch.float32)
+            sample = torch.zeros(SAMPLE_FRAMES, 3, 180, 320, dtype=torch.float32)
             for idx_frame in range(0, num_frames):
                 # idx_frame+1 because frames start from 1.  e.g. 1.jpg
                 frame = Image.open(os.path.join(DATA_ROOT, VIDEO_FRAMES, dir, str(idx_frame+1)+'.jpg')).convert('RGB')
                 frame = transform(frame).to(dtype=torch.float32)
-
-                sample[idx_frame%CHUNK_SIZE] = frame
-                if (idx_frame%CHUNK_SIZE) == (CHUNK_SIZE-1):
-                    batch[count_sample%BATCH_SIZE] = sample
-                    count_sample += 1
-                    sample = torch.zeros(CHUNK_SIZE, 3, 180, 320, dtype=torch.float32)
-
-                if count_sample%BATCH_SIZE == 0 and count_sample != 0:
-                    batch = batch.permute(0, 2, 1, 3, 4).to(device)
+                sample[idx_frame%SAMPLE_FRAMES] = frame
+                if idx_frame%SAMPLE_FRAMES == SAMPLE_FRAMES-1:
+                    sample = sample.permute(1, 0, 2, 3).unsqueeze(0).to(device)
                     # forward pass
-                    feat_vects = model(batch)       # feat_vects.shape == (batch_size, feat_vect_dim)
-                    start = count_sample - BATCH_SIZE
-                    end = count_sample
-                    feat_vects_video[start:end] = feat_vects
-                    batch = torch.zeros(BATCH_SIZE, CHUNK_SIZE, 3, 180, 320, dtype=torch.float32)
-
-            # if count_sample%BATCH_SIZE != 0 there will be another batch with size less than BATCH_SIZE
-            if count_sample%BATCH_SIZE != 0:
-                batch = batch[:count_sample%32]   # drop zeros
-                batch = batch.permute(0, 2, 1, 3, 4).to(device)
-                # forward pass
-                feat_vects = model(batch)  # feat_vects.shape == (batch_size, feat_vect_dim)
-                start = count_sample - BATCH_SIZE
-                end = count_sample
-                feat_vects_video[start:end] = feat_vects
+                    feat_vect = model(sample)     # TODO: load a batch instead of a single sample
+                    feat_vects_video[idx_frame//SAMPLE_FRAMES] = feat_vect.squeeze(0)
+                    sample = torch.zeros(SAMPLE_FRAMES, 3, 180, 320, dtype=torch.float32)
 
             np.save(os.path.join(DATA_ROOT, VIDEO_FEATURES, str(dir)+'.npy'), feat_vects_video.numpy())
 
