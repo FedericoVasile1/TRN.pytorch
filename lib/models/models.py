5f3a0ba305af03b82e598a29634ec3671b26a996
FedericoVasile1 <50639319+FedericoVasile1@users.noreply.github.com>
2020-07-07 23:47:27 +0200

    minor fix

--- lib/models/models.py
+++ lib/models/models.py
@@ -2,9 +2,9 @@ from .generalized_trn import GeneralizedTRN
 from .lstm import LSTMmodel
 from .mytrn import MyTRN
 from .cnn3d import CNN3D
-from .fc_actiondetector import FC_AC
 from .futurelstm import FutureLSTM
 from .disciminator_lstm import DiscriminatorLSTM
+from .discriminator_cnn3d import DiscriminatorCNN3D
 
 _META_ARCHITECTURES = {
     'TRN': GeneralizedTRN,
@@ -14,6 +14,7 @@ _META_ARCHITECTURES = {
     'RESNET2+1D': CNN3D,
     'FUTURELSTM': FutureLSTM,
     'DISCRIMINATORLSTM': DiscriminatorLSTM,
+    'DISCRIMINATORCNN3D': DiscriminatorCNN3D,
 }
 
 def build_model(args):
