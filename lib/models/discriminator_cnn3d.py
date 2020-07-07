66935e6db4b84ae5d912a4958d447c0010298c8f
FedericoVasile1 <50639319+FedericoVasile1@users.noreply.github.com>
2020-07-07 23:43:50 +0200

    added discriminator cnn3d

---
+++ lib/models/discriminator_cnn3d.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+from .feature_extractor import build_feature_extractor
+
+class DiscriminatorCNN3D(nn.Module):
+    def __init__(self, args):
+        super(DiscriminatorCNN3D, self).__init__()
+        if args.camera_feature != 'video_frames_24fps':
+            raise Exception('Wrong camera_feature option')
+
+        if args.model == 'RESNET2+1D':
+            self.feature_extractor = models.video.r2plus1d_18(pretrained=True)
+            # freeze all but last(i.e. layer4) conv layers
+            for param in self.feature_extractor.parameters():
+                param.requires_grad = False
+            for param in self.feature_extractor.layer3.parameters():
+                param.requires_grad = True
+            for param in self.feature_extractor.layer4.parameters():
+                param.requires_grad = True
+            self.feature_extractor.fc = nn.Linear(self.feature_extractor.fc.in_features,
+                                                  args.num_classes)  # requires_grad == True by default
+        else:
+            raise Exception('Wrong model option, ' + args.model + ' model is not supported')
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        return x
