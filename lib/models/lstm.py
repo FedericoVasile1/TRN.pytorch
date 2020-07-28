e5b4079ee3d933c062d6ab4920fa7ce66de2b1db
FedericoVasile1 <50639319+FedericoVasile1@users.noreply.github.com>
2020-07-28 15:42:40 +0200

    added temporal dilated convolution in lstm model

--- lib/models/lstm.py
+++ lib/models/lstm.py
@@ -3,6 +3,43 @@ import torch.nn as nn
 
 from .feature_extractor import build_feature_extractor
 
+
+def CausalConv1d(in_channels, out_channels, kernel_size, dilation=1, **kwargs):
+    pad = (kernel_size - 1) * dilation
+    return nn.Conv1d(in_channels, out_channels, kernel_size, padding=pad, dilation=dilation, **kwargs)
+
+class TCN(nn.Module):
+    def __init__(self, args):
+        super(TCN, self).__init__()
+        self.conv1 = nn.Sequential(
+            CausalConv1d(1, 1, 2, 1),
+            nn.ReLU(),
+            nn.Dropout(args.dropout),
+        )
+        self.conv2 = nn.Sequential(
+            CausalConv1d(1, 1, 2, 2),
+            nn.ReLU(),
+            nn.Dropout(args.dropout),
+        )
+        self.conv3 = nn.Sequential(
+            CausalConv1d(1, 1, 2, 4),
+            nn.ReLU(),
+            nn.Dropout(args.dropout),
+        )
+
+    def forward(self, x):
+        x = x.unsqueeze(1)
+        out1 = self.conv1(x)
+        x = torch.cat((x, torch.ones(out1.shape[0], 1, 1).to(out1.device)), dim=2)
+        out1 += x
+        out2 = self.conv2(out1)
+        out1 = torch.cat((out1, torch.ones(out2.shape[0], 1, 2).to(out2.device)), dim=2)
+        out2 += out1
+        out3 = self.conv3(out2)
+        out2 = torch.cat((out2, torch.ones(out3.shape[0], 1, 4).to(out3.device)), dim=2)
+        out3 += out2
+        return out3.squeeze(1)
+
 class LSTMmodel(nn.Module):
     def __init__(self, args):
         super(LSTMmodel, self).__init__()
@@ -12,8 +49,10 @@ class LSTMmodel(nn.Module):
 
         self.feature_extractor = build_feature_extractor(args)
 
+        self.tcn = TCN(args)
+
         self.drop = nn.Dropout(args.dropout)
-        self.lstm = nn.LSTMCell(self.feature_extractor.fusion_size, self.hidden_size)
+        self.lstm = nn.LSTMCell(self.feature_extractor.fusion_size + 7, self.hidden_size)
         self.classifier = nn.Linear(self.hidden_size, self.num_classes)
 
     def forward(self, x):
@@ -24,6 +63,8 @@ class LSTMmodel(nn.Module):
         for step in range(self.enc_steps):
             x_t = x[:, step]
             out = self.feature_extractor(x_t, torch.zeros(1))  # second input is optical flow, in our case will not be used
+            out = self.tcn(out)
+
             h_n, c_n = self.lstm(out, (h_n, c_n))
             out = self.classifier(self.drop(h_n))  # out.shape == (batch_size, num_classes)
 
