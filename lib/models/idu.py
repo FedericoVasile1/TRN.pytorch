6fd0970e0029ec0b57116f21a95865b45be09687
FedericoVasile1 <50639319+FedericoVasile1@users.noreply.github.com>
2020-07-30 11:57:00 +0200

    minor fixes

--- lib/models/idu.py
+++ lib/models/idu.py
@@ -3,14 +3,13 @@ import torch.nn as nn
 from torch.nn.parameter import Parameter
 import torch.nn.functional as F
 
-from models.gru import GRU
-
 import math
 
 class IDUCell(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
+    def __init__(self, input_size, hidden_size, num_classes, steps):
         super(IDUCell, self).__init__()
         self.num_classes = num_classes
+        self.steps = steps
 
         self.Wxe = Parameter(torch.randn(input_size, hidden_size)).div(math.sqrt(input_size))
         self.bxe = Parameter(torch.zeros(hidden_size))
@@ -44,7 +43,6 @@ class IDUCell(nn.Module):
                 xte, xt0: the outputs of the early embedding module, for current timestep and current
                             information, both of shape (batch_size, hidden_size)
         '''
-
         xte = F.relu(xt.mm(self.Wxe) + self.bxe)
         x0e = F.relu(x0.mm(self.Wxe) + self.bxe)
 
@@ -72,17 +70,16 @@ class IDUCell(nn.Module):
                 ptes, p0es: prpbability distributions, of shape (batch_size, enc_steps, num_classes)
                 xtes, x0es: early embedding features, of shape (batch_size, enc_steps, hidden_size)
         '''
-        batch_size, steps, _ = x.shape
-        hidden_size = h0.shape[1]
-        hts = torch.zeros(batch_size, steps, hidden_size).to(dtype=x.dtype)
-        ptes = torch.zeros(batch_size, steps, self.num_classes).to(dtype=x.dtype)
-        p0es = torch.zeros(batch_size, steps, self.num_classes).to(dtype=x.dtype)
-        xtes = torch.zeros(batch_size, steps, hidden_size).to(dtype=x.dtype)
-        x0es = torch.zeros(batch_size, steps, hidden_size).to(dtype=x.dtype)
+        batch_size, hidden_size = h0.shape
+        hts = torch.zeros(batch_size, self.steps, hidden_size).to(dtype=x.dtype)
+        ptes = torch.zeros(batch_size, self.steps, self.num_classes).to(dtype=x.dtype)
+        p0es = torch.zeros(batch_size, self.steps, self.num_classes).to(dtype=x.dtype)
+        xtes = torch.zeros(batch_size, self.steps, hidden_size).to(dtype=x.dtype)
+        x0es = torch.zeros(batch_size, self.steps, hidden_size).to(dtype=x.dtype)
 
         x0 = x[:, -1]        # last step features is the current information
         h_t = h0
-        for step_t in range(steps):
+        for step_t in range(self.steps):
             x_t = x[:, step_t]
             h_t, pte, p0e, xte, x0e = self.step(x_t, x0, h_t)
 
