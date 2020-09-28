056b986529fd4270c872cd254401d06bd11ce8b0
FedericoVasile1 <50639319+FedericoVasile1@users.noreply.github.com>
2020-09-28 20:47:24 +0200

    added cnn3d evaluation

--- tools/cnn3d_judo/eval_allframes.py
+++ tools/cnn3d_judo/eval_allframes.py
@@ -2,8 +2,8 @@ import os
 import os.path as osp
 import sys
 import time
-import json
 import random
+import json
 from datetime import datetime
 import numpy as np
 import pandas as pd
@@ -14,18 +14,18 @@ from sklearn.metrics import confusion_matrix
 
 import torch
 import torch.nn as nn
+from torchvision import transforms
 from torch.utils.tensorboard import SummaryWriter
+from PIL import Image
 
 sys.path.append(os.getcwd())
 import _init_paths
 import utils as utl
 from lib.utils.visualize import show_video_predictions
-from configs.thumos import parse_trn_args as parse_args
+from configs.judo import parse_trn_args as parse_args
 from lib.utils.visualize import plot_perclassap_bar, plot_to_image, add_pr_curve_tensorboard
 from models import build_model
-
-def to_device(x, device):
-    return x.unsqueeze(0).to(device)
+from lib.datasets.thumos_data_layer_e2e import I3DNormalization
 
 def main(args):
     os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
@@ -44,6 +44,23 @@ def main(args):
 
     softmax = nn.Softmax(dim=1).to(device)
 
+    if args.feature_extractor == 'I3D':
+        transform = transforms.Compose([
+            transforms.Resize((224, 320)),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            I3DNormalization(),
+        ])
+    elif args.feature_extractor == 'RESNET2+1D':
+        transform = transforms.Compose([
+            transforms.Resize((224, 320)),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize([0.43216, 0.394666, 0.37645], [0.22803, 0.22145, 0.216989])
+        ])
+    else:
+        raise Exception('Wrong feature_extractor option, ' + args.feature_extractor + ' is not supported')
+
     if args.show_predictions:
         count_frames = 0
         if args.seed_show_predictions != -1:
@@ -52,20 +69,62 @@ def main(args):
     for session_idx, session in enumerate(args.test_session_set, start=1):
         start = time.time()
         with torch.set_grad_enabled(False):
-            camera_inputs = np.load(osp.join(args.data_root, args.camera_feature, session+'.npy'), mmap_mode='r')
-            camera_inputs = torch.as_tensor(camera_inputs.astype(np.float32))
-            target = np.load(osp.join(args.data_root, 'target', session+'.npy'))
+            original_target = np.load(osp.join(args.data_root, 'target_frames_25fps', session + '.npy'))
+            # round to multiple of CHUNK_SIZE
+            num_frames = original_target.shape[0]
+            num_frames = num_frames - (num_frames % args.chunk_size)
+            original_target = original_target[:num_frames]
+            # For each chunk, take only the central frame
+            target = original_target[args.chunk_size // 2::args.chunk_size]
 
-            for l in range(target.shape[0]):
-                if l % args.enc_steps == 0:
-                    enc_h_n = torch.zeros(1, model.hidden_size, device=device, dtype=camera_inputs.dtype)
-                    enc_c_n = torch.zeros(1, model.hidden_size, device=device, dtype=camera_inputs.dtype)
+            batch_samples = None
+            for count in range(target.shape[0]):
+                idx_central_frame = count * args.chunk_size + (args.chunk_size // 2)
+                start_f = idx_central_frame - args.chunk_size // 2
+                end_f = idx_central_frame + args.chunk_size // 2
+                for idx_frame in range(start_f, end_f):
+                    frame = Image.open(osp.join(args.data_root,
+                                                args.camera_feature,
+                                                session,
+                                                str(idx_frame + 1) + '.jpg')).convert('RGB')
+                    frame = transform(frame)
 
-                camera_input = to_device(camera_inputs[l], device)
-                enc_score, enc_h_n, enc_c_n = model.step(camera_input, torch.zeros(1), enc_h_n, enc_c_n)
+                    if batch_samples is None:
+                        batch_samples = torch.zeros(args.batch_size,
+                                                    args.chunk_size,
+                                                    frame.shape[0],
+                                                    frame.shape[1],
+                                                    frame.shape[2],
+                                                    dtype=torch.float32)
+                    batch_samples[count % args.batch_size, idx_frame - start_f] = frame
 
-                enc_score_metrics.append(softmax(enc_score).cpu().numpy()[0])
-                enc_target_metrics.append(target[l])
+                if count % args.batch_size == args.batch_size - 1:
+                    # forward pass
+                    batch_samples = batch_samples.permute(0, 2, 1, 3, 4)
+                    batch_samples = batch_samples.to(device)
+                    scores = model.forward(batch_samples)
+
+                    scores = softmax(scores).cpu().detach().numpy()
+                    for i in range(scores.shape[0]):
+                        for c in range(args.chunk_size):
+                            enc_score_metrics.append(scores[i])
+                            enc_target_metrics.append(
+                                original_target[((count+1) - args.batch_size + i) * args.chunk_size + c])
+
+                    batch_samples = None
+        # do the last forward pass, because there will probably be the last batch with samples < batch_size
+        if batch_samples is not None:
+            # forward pass
+            batch_samples = batch_samples.permute(0, 2, 1, 3, 4)
+            batch_samples = batch_samples.to(device)
+            scores = model.forward(batch_samples)
+
+            scores = softmax(scores).cpu().detach().numpy()
+            for i in range(scores.shape[0]):
+                for c in range(args.chunk_size):
+                    enc_score_metrics.append(scores[i])
+                    enc_target_metrics.append(
+                        original_target[((count + 1) - args.batch_size + i) * args.chunk_size + c])
 
         end = time.time()
 
@@ -73,13 +132,12 @@ def main(args):
                                                                                  session_idx,
                                                                                  len(args.test_session_set),
                                                                                  end - start))
-
         if args.show_predictions:
-            show_video_predictions(args,
-                                   session,
-                                   enc_target_metrics[count_frames:count_frames + target.shape[0]],
-                                   enc_score_metrics[count_frames:count_frames + target.shape[0]])
-            count_frames += target.shape[0]
+            utl.show_video_predictions(args,
+                                       session,
+                                       enc_score_metrics[count_frames:count_frames + original_target.shape[0]],
+                                       enc_target_metrics[count_frames:count_frames + original_target.shape[0]])
+            count_frames += original_target.shape[0]
 
     save_dir = osp.dirname(args.checkpoint)
     result_file  = osp.basename(args.checkpoint).replace('.pth', '.json')
@@ -89,8 +147,9 @@ def main(args):
                                   enc_target_metrics,
                                   save_dir,
                                   result_file,
-                                  ignore_class=[0,21],
+                                  ignore_class=[0],
                                   save=True,
+                                  switch=False,
                                   verbose=True)
 
     writer = SummaryWriter()
@@ -101,23 +160,18 @@ def main(args):
         per_class_ap = json.load(f)['AP']
     for class_name in per_class_ap:
         per_class_ap[class_name] = round(per_class_ap[class_name], 2)
-    figure = plot_perclassap_bar(per_class_ap.keys(), per_class_ap.values(), title=args.dataset+': per-class AP')
+    figure = plot_perclassap_bar(per_class_ap.keys(), per_class_ap.values(), title=args.dataset + ': per-class AP')
     figure = plot_to_image(figure)
-    writer.add_image(args.dataset+': per-class AP', np.transpose(figure, (2, 0, 1)), 0)
+    writer.add_image(args.dataset + ': per-class AP', np.transpose(figure, (2, 0, 1)), 0)
     writer.close()
 
     enc_score_metrics = np.array(enc_score_metrics)
-    # Assign cliff diving (5) as diving (8)
-    switch_index = np.where(enc_score_metrics[:, 5] > enc_score_metrics[:, 8])[0]
-    enc_score_metrics[switch_index, 8] = enc_score_metrics[switch_index, 5]
     # Prepare variables
     enc_score_metrics = torch.tensor(enc_score_metrics)  # shape == (num_videos * num_frames_in_video, num_classes)
     enc_target_metrics = torch.max(torch.tensor(enc_target_metrics), 1)[1]  # shape == (num_videos * num_frames_in_video)
 
     # Log precision recall curve for encoder
     for idx_class in range(len(args.class_index)):
-        if idx_class == 20 or idx_class == 5:
-            continue  # ignore ambiguos class and cliff diving class
         add_pr_curve_tensorboard(writer,
                                  args.class_index[idx_class],
                                  idx_class,
@@ -127,8 +181,6 @@ def main(args):
 
     # For each sample, takes the predicted class based on his scores
     enc_pred_metrics = torch.max(enc_score_metrics, 1)[1]
-
-    args.class_index.pop(5)
 
     # Log unnormalized confusion matrix for encoder
     conf_mat = confusion_matrix(enc_target_metrics, enc_pred_metrics)
