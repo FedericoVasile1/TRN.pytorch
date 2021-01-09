5c2287ab8ae9f7065b56fcb588503661e5b14e53
FedericoVasile1 <50639319+FedericoVasile1@users.noreply.github.com>
2021-01-09 10:54:15 +0100

    added positional encoding to transformer

--- lib/models/transformer.py
+++ lib/models/transformer.py
@@ -1,7 +1,36 @@
+import math
+
 import torch
 import torch.nn as nn
+from torch.autograd import Variable
 
 from .feature_extractor import build_feature_extractor
+
+
+class PositionalEncoder(nn.Module):
+    def __init__(self, d_model, max_seq_len=80):
+        super(PositionalEncoder, self).__init__()
+        self.d_model = d_model
+        pe = torch.zeros(max_seq_len, d_model)
+        for pos in range(max_seq_len):
+            for i in range(0, d_model, 2):
+                pe[pos, i] = \
+                    math.sin(pos / (10000 ** ((2 * i) / d_model)))
+                pe[pos, i + 1] = \
+                    math.cos(pos / (10000 ** ((2 * (i + 1)) / d_model)))
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        # x.shape == (batch_size, seq_len, num_features)
+        # make embeddings relatively larger
+        x = x * math.sqrt(self.d_model)
+        # add constant to embedding
+        seq_len = x.size(1)
+        z = Variable(self.pe[:, :seq_len], requires_grad=False)
+        x = x + z
+        return x
+
 
 class Transformer(nn.Module):
     def __init__(self, args):
@@ -14,6 +43,7 @@ class Transformer(nn.Module):
         self.feature_extractor = build_feature_extractor(args)
         self.d_model = self.feature_extractor.fusion_size
 
+        self.pe = PositionalEncoder(self.d_model, self.steps)
         encoder_layer = nn.TransformerEncoderLayer(d_model=self.d_model, nhead=self.nhead, dropout=args.dropout)
         self.transformer = nn.TransformerEncoder(encoder_layer, num_layers=self.num_layers)
 
@@ -28,6 +58,7 @@ class Transformer(nn.Module):
         for step in range(self.steps):
             transf_x[:, step] = self.feature_extractor(x[:, step])
 
+        transf_x = self.pe(transf_x)
         transf_x = self.transformer(transf_x.transpose(0, 1)).transpose(0, 1)
 
         scores = self.classifier(transf_x)
